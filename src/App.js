486556dbf3f58a73abf1537382a89e00de646e54
Juan Buendia <juan.buendia@usantoto.edu.co>
2021-11-03 16:06:23 -0500

    Creación proyecto de react.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Listado from './componenteLista';
+
+function App(){
+  const [nombrePersona, setNombrePersona] = useState('');
+  const saludarFunction=()=>{
+    let aux = document.getElementById('txtName').value;
+    alert("Hola " + aux);
+    console.log("OK")
+  }
+  return (
+    <div className="App">
+      <h1>HOLA MUNDO</h1>      
+      <input type="text" id="txtName" onChange={e=> setNombrePersona(e.target.value)}/>
+      <button id="btnSaludo" onClick={saludarFunction}>Saludar</button>
+      <p>{nombrePersona}</p>
+      <Listado></Listado>
+    </div>
+  );
+}
+
+export default App;
