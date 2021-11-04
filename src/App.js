995c0768a6dd660587a8a8f9a2afa6d6d7a23678
Juan Buendia <juan.buendia@usantoto.edu.co>
2021-11-04 16:30:33 -0500

    Proyecto base por si la cagamos simplemente hacer pull.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import './App.css';
-import { useState } from 'react';
-import Listado from './componenteLista';
 
-function App(){
-  const [nombrePersona, setNombrePersona] = useState('');
-  const saludarFunction=()=>{
-    let aux = document.getElementById('txtName').value;
-    alert("Hola " + aux);
-    console.log("OK")
-  }
+function App() {
   return (
     <div className="App">
-      <h1>HOLA MUNDO</h1>      
-      <input type="text" id="txtName" onChange={e=> setNombrePersona(e.target.value)}/>
-      <button id="btnSaludo" onClick={saludarFunction}>Saludar</button>
-      <p>{nombrePersona}</p>
-      <Listado></Listado>
+      AAAAAAAAAAAAAAAAAAAAAA - Si funciona
     </div>
   );
 }
