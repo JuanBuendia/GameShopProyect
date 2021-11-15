c4b7eb433ca63d6a7849c2df288fd45e1307f44e
Juan Buendia <juan.buendia@usantoto.edu.co>
2021-11-14 20:18:37 -0500

    Cambio a steam api

--- src/pages/components/Games/index.jsx
+++ src/pages/components/Games/index.jsx
@@ -1,32 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { Button, Card, Row } from "react-bootstrap";
 
 function Games() {
   const [listaJuegos, setListaJuegos] = useState([]);
 
-  const options = {
+  var options = {
     method: "GET",
-    url: "https://cheapshark-game-deals.p.rapidapi.com/deals",
-    params: {
-      lowerPrice: "0",
-      steamRating: "0",
-      title: "batman",
-      desc: "0",
-      output: "json",
-      steamworks: "0",
-      sortBy: "Deal Rating",
-      AAA: "0",
-      pageSize: "60",
-      exact: "0",
-      upperPrice: "50",
-      pageNumber: "0",
-      onSale: "0",
-      metacritic: "0",
-      storeID: "1,2,3",
-    },
+    url: "https://steam-store-data.p.rapidapi.com/api/featured/",
     headers: {
-      "x-rapidapi-host": "cheapshark-game-deals.p.rapidapi.com",
-      "x-rapidapi-key": "f60d33d507mshe74872ebd7754f4p11e444jsn30f38184cbe3",
+      "x-rapidapi-host": "steam-store-data.p.rapidapi.com",
+      "x-rapidapi-key": "key-goes-here",
     },
   };
 
@@ -34,8 +18,8 @@ function Games() {
     axios
       .request(options)
       .then(function (response) {
-        console.log(response.data);
-        setListaJuegos(response.data);
+        console.log(response.data.featured_win);
+        setListaJuegos(response.data.featured_win);
       })
       .catch(function (error) {
         console.error(error);
@@ -44,37 +28,26 @@ function Games() {
 
   useEffect(() => {
     fetchData();
-  },);
+  });
 
   return (
-    <>
-      <div className="app" class="container-fluid">
-      <div class="col-2"></div>
-      <div class="col-8">
-      <div class="row row-cols-1 row-cols-md-3 g-4">
-          
-          {listaJuegos.map((item) => (
-            <div class="col align-content-center">
-              <div class="card" style={{ width: 300, height: 300 }}>
-                <img
-                  src={item.thumb}
-                  class="card-img-top imagencita"
-                  alt=""
-                  style={{ width: "auto", height: 200 }}
-                />
-                <div class="card-body">
-                  <h5 class="card-title">{item.title}</h5>
-                  <p class="card-text">{item.steamRatingText}</p>
-                </div>
-              </div>
-            </div>
-          ))}          
-        </div>
-      </div>
-        
-        <div class="col-2"></div>
-      </div>
-    </>
+    <Row xs={1} md={2} className="g-4">
+      {listaJuegos.map((item) => (
+        <Card
+          className="text-center bg-dark text-white g-4"
+          style={{ width: "18rem" }}
+        >
+          <Card.Img variant="top" src={item.large_capsule_image} />
+          <Card.Body>
+            <Card.Title>{item.name}</Card.Title>
+            <Card.Text>
+              {item.currency} {item.original_price}
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      ))}
+    </Row>
   );
 }
 
