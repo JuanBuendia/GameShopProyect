8d48c4695b72a0ef84092239bdcae74140ef91b3
Juan Buendia <juan.buendia@usantoto.edu.co>
2021-11-23 17:35:00 -0500

    Primer intento de enlace home y juego

--- src/pages/components/home/index.jsx
+++ src/pages/components/home/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Carousel, Button, Row, Col, Card } from "react-bootstrap";
-import axios from "axios"; 
-import { useEffect, useState } from "react"; 
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [listaJuegos, setListaJuegos] = useState([]);
@@ -29,7 +30,7 @@ function Home() {
 
   useEffect(() => {
     fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <>
@@ -77,7 +78,11 @@ function Home() {
                   <Card.Text>
                     {item.currency} {item.original_price}
                   </Card.Text>
-                  <Button variant="primary">Comprar</Button>
+                  <Button variant="dark">
+                    <Link to={`/gameDetail/${item.id}`} className="text-white b">
+                      Ir a la página del juego
+                    </Link>
+                  </Button>
                 </Card.Body>
               </Card>
             </Col>
